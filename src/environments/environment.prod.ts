b85640a6917be527018898eb460e53f249a71f22
Manuel Japon <efrain.japon@gmail.com>
2020-04-27 15:29:24 -0500

    version telemedicina beta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     baseUrlEndPoint: 'https://www.fusay.org/api',
-    facebookLoginApp: '606945676568898',
-    googleLoginApp: '702999941401-ql2c4rufsliivam2f7hub633brvicgk0.apps.googleusercontent.com'
+
 };
