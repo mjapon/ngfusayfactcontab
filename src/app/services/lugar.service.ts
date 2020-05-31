1f79ca526c88f2b84d9c64895a686d022699c274
Manuel Japon <efrain.japon@gmail.com>
2020-05-30 20:42:11 -0500

    version inicial de historias clinicas

---
+++ src/app/services/lugar.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {BaseService} from "./base-service";
+import {HttpClient} from "@angular/common/http";
+import {LocalStorageService} from "./local-storage.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LugarService extends BaseService {
+
+    constructor(
+        private http: HttpClient,
+        protected localStrgServ: LocalStorageService
+    ) {
+        super('/tlugar', localStrgServ);
+    }
+
+    listarTodos(): Observable<any> {
+        console.log('Emntra en listar todos');
+        const endopoint = this.urlEndPoint;
+        const httpOptions = this.getHttpOptionsToken({accion: 'listar'});
+        return this.doGet(this.http, endopoint, httpOptions);
+    }
+
+}
