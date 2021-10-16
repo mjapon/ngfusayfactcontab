c1ac80379e7dfe08fa66cbeddb68f210a031ff36
manueljapon <efrain.japon@gmail.com>
2021-10-15 19:18:52 -0500

    cleanup

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrlEndPoint: 'https://mavil.site/api',
-    baseUrlDwfile: 'https://mavil.site/getrxdoc',
-    baseUrlDwfileNode: 'https://mavil.site/getrxdocnode',
-    tomcat: 'https://mavil.site/tomcat/imprentas',
-    socket: 'https://mavil.site'
-};
